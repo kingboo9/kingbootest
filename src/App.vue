152ff23612f7cd940cb6eb503b6f14dfdfda058a
kingboo9 <bongsus@pmgroup.co.kr>
2022-03-18 11:20:26 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <NavBarName />
+    <TableName />
+</template>
+
+<script>
+import NavBarName from './components/NavBar.vue'
+import TableName from './components/Table.vue'
+
+export default {
+    name: 'App',
+    components: {
+        NavBarName,
+        TableName
+    }
+}
+</script>
